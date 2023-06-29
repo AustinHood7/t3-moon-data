380b7c115a795ce6abc74d74684b94c9451bc349
austin.hood <Austinmhood@live.com>
2023-06-29 02:36:41 -0400

    ESLint has a problem with ' so I need to add exceptions when they are used for text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,9 +47,9 @@ export default function Home() {
                       <div className="text-xl text-white p-4 rounded-xl bg-white/10 hover:bg-white/20 mb-16 md:mb-0 transition duration-500"  id="dropdown">
                         <div className="h-[25vh] overflow-scroll">
                           The moon phase angle, often referred to as the elongation, is the angle between the moon and the sun as viewed from Earth. 
-                          It's a way to quantify the phases of the moon. It essentially measures how much of the moon's face is illuminated by the sun from the perspective 
-                          of an observer on Earth.Astronomers and astrophotographers often use the moon phase angle to plan observations and photographs. 
-                          It can also be of interest to anyone observing the moon, as the moon's appearance can change dramatically depending on the phase angle.
+                          It\'s a way to quantify the phases of the moon. It essentially measures how much of the moon\'s face is illuminated by the sun from the perspective 
+                          of an observer on Earth. Astronomers and astrophotographers often use the moon phase angle to plan observations and photographs. 
+                          It can also be of interest to anyone observing the moon, as the moon\'s appearance can change dramatically depending on the phase angle.
                         </div> 
                       </div> 
                   </div>
@@ -59,11 +59,11 @@ export default function Home() {
                     </div>
                       <div className="text-xl text-white p-4 rounded-xl bg-white/10 hover:bg-white/20 mb-16 md:mb-0" id="dropdown">
                         <div className="h-[25vh] overflow-scroll">
-                          The Moon phase age is the number of days since the last New Moon. It indicates the Moon's progress in its cycle and ranges between 0 to approximately 
+                          The Moon phase age is the number of days since the last New Moon. It indicates the Moon\'s progress in its cycle and ranges between 0 to approximately 
                           29.5 days. The New Moon phase occurs when the Moon is positioned between the Earth and the Sun, making it almost invisible to us here on Earth.The Moon 
-                          phase age can be used for a variety of purposes. For example, astronomers, astrophotographers, and stargazers use it to track the Moon's phases and plan 
+                          phase age can be used for a variety of purposes. For example, astronomers, astrophotographers, and stargazers use it to track the Moon\'s phases and plan 
                           observations or photography sessions. The Moon phase age also plays a role in various cultural and religious practices and events that are based on the lunar 
-                          calendar.It's also worth mentioning that the phase age can help in understanding tidal activity since the gravitational pull that causes tides is strongest 
+                          calendar.It\'s also worth mentioning that the phase age can help in understanding tidal activity since the gravitational pull that causes tides is strongest 
                           when the Moon is at the New Moon or Full Moon phase, leading to higher than normal tides known as spring tides. During the First Quarter and Last Quarter phases,
                           the pull is weaker, resulting in lower than average tides known as neap tides.
                         </div> 
@@ -77,7 +77,7 @@ export default function Home() {
 
                       <div className="h-[25vh] overflow-scroll">
                         This is a normalized measure of the lunar cycle, where the New Moon is 0, First Quarter is 0.25, Full Moon is 0.5, and Last Quarter is 0.75. 
-                        The value wraps back to 0 after reaching 1. The value of the phase indicates the Moon's position in its orbit around the Earth. This is a continuous 
+                        The value wraps back to 0 after reaching 1. The value of the phase indicates the Moon\'s position in its orbit around the Earth. This is a continuous 
                         measure, unlike the phase name which describes discrete segments of the lunar cycle New Moon, Waxing Crescent, First Quarter, Waxing Gibbous, Full Moon, 
                         Waning Gibbous, Last Quarter, Waning Crescent.
                       </div> 
