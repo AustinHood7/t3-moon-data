ced5a92a9a688ad8cc3c70b1425249a156097feb
austin.hood <Austinmhood@live.com>
2023-06-29 02:27:25 -0400

    Made images of moons dynamic and change when the data from api is updated to new moon phase, also styled the landing page further

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'radial-gradient-start': 'rgba(252,211,77,0.64)', // the start color of your radial gradient
+        'radial-gradient-end': 'rgba(28,25,23,1)', // the end color of your radial gradient
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
